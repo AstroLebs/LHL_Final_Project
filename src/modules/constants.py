262b6f81ad362158b5d6fe6e6ed97050fbecb6f5
astrolebs <calebgward@gmail.com>
2022-11-30 23:08:14 -0800

    Add FBREF data

--- src/modules/constants.py
+++ src/modules/constants.py
@@ -1,2 +1,45 @@
 # API URLS
 FPL_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
+FPL_HIST = (
+    "https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/"
+)
+
+FBREF_URLS = "https://fbref.com/en/comps/9/2021-2022/2021-2022-Premier-League-Stats"
+FBREF_SEARCH_URL = "https://fbref.com/search/search.fcgi?search="
+FBREF_PLAYER_LABELS = [
+    "standard_stats",
+    "shooting",
+    "passing",
+    "pass_types",
+    "goal_shot",
+    "defensive",
+    "possession",
+    "playing_time",
+    "misc",
+]
+FBREF_TEAM_LABELS = [
+    "regular_season_overall",
+    "regular_season_homeaway",
+    "standard_squad",
+    "standard_opponent",
+    "goalkeeping_squad",
+    "goalkeeping_opponent",
+    "goalkeeping_adv_squad",
+    "goalkeeping_adv_opponent",
+    "shooting_squad",
+    "shooting_opponent",
+    "passing_squad",
+    "passing_opponent",
+    "pass_type_squad",
+    "pass_type_opponent",
+    "goal_shot_creation_squad",
+    "goal_shot_creation_opponent",
+    "defensive_action_squad",
+    "defensive_action_opponent",
+    "possession_squad",
+    "possession_opponent",
+    "playtime_squad",
+    "playtime_opponent",
+    "misc_squad",
+    "misc_opponent",
+]
