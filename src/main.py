21c11cc4a070cc2a76833becb0c21dd2c627714f
astrolebs <calebgward@gmail.com>
2023-01-09 16:16:29 -0800

    Removal of old files

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import modules
+
+def main():
+
+    
+    # Load Datasets
+    # Load / Run Year Start Model
+    # Optimize Year Start Result
+    # Return
+    # Check for New Data
+    # Run Week-to-Week Model to look for Transfers
+    # Optimize Transfer Suggestions
+    # Return
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
